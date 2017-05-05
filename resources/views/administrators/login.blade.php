3ad171de5723636a4aa63a8368be6888fcd86869
Yasmin Montoya <yaz.lorena@gmail.com>
2017-05-05 08:48:14 -0500

    Archivo con contraseñas de acceso

--- resources/views/administrators/login.blade.php
+++ resources/views/administrators/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login Admin</div>
+                <div class="panel-heading">Login Administrador</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('admin.login.submit') }}">
                         {{ csrf_field() }}
